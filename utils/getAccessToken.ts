b5f7f307dfb5f1969dd63f13a3cada7a75ebe7ac
Toh Hong Xiang <tohhongxiang@gmail.com>
2021-09-01 14:16:02 +0800

    cfix: Update version

--- utils/getAccessToken.ts
+++ utils/getAccessToken.ts
@@ -1,15 +1,15 @@
 import { API_URL } from "../constants"
 
-export default async function getAccessToken() : Promise<string> {
-    const { data: { access_token } } = await fetch(`${API_URL}/auth/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email: process.env.DIRECTUS_EMAIL,
-          password: process.env.DIRECTUS_PASSWORD
-        })
-      }).then(res => res.json())
-    return access_token
+export default async function getAccessToken(): Promise<string> {
+  const { data: { access_token } } = await fetch(`${API_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: process.env.DIRECTUS_EMAIL,
+      password: process.env.DIRECTUS_PASSWORD
+    })
+  }).then(res => res.json())
+  return access_token
 }
